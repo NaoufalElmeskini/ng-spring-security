964e04fd32fa206381463c8cc5801b58d771ceaa
tintin <tintin.vocard@gmail.com>
2025-08-23 09:58:04 +0200

    initial commit

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FormDemoComponent} from './form-demo/form-demo.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, FormDemoComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+}
